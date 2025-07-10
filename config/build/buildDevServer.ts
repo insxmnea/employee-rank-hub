28b29fd75e6dfa587a75c817f646c1ce4cd7cd38
Konstantin <kostya.svetashov.01@gmail.com>
2025-07-10 16:32:13 +0400

    feat: add react router, use lazy components

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,5 +4,6 @@ import type { Configuration as DevServerConfiguration } from "webpack-dev-server
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
   return {
     port: options.port,
+    historyApiFallback: true,
   };
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { Link, Route, Routes } from "react-router";
 import { Counter } from "./components/Counter";
 import "./index.scss";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { Suspense } from "react";
 
 export const App = () => {
   return (
     <div className="app">
       <Counter />
+      <Link to={"/"}>Main page</Link>
+      <Link to={"/about"}>About page</Link>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
